8eb492ef95b14790a557bc6b7a2f3f90da49f74c
oxlm <lxq_69697769@163.com>
2013-03-31 22:59:00 +0800

    Create buttons.h

---
+++ buttons.h
@@ -0,0 +1,7 @@
+#ifndef __BUTTONS_H
+#define __BUTTONS_H
+
+enum Button {volumeUp, volumeDown, usbMode, spdifMode,analogMode, muteMode, shutdownMode, surround0, surround1};
+void Buttons(Button button);  
+
+#endif
